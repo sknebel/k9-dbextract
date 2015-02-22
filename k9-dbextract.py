0d7bc22156026f9e61427528bd113eb2146bc102
Sven Knebel <sknebel@users.noreply.github.com>
2015-02-23 00:04:02 +0100

    Allow for multiple mail profiles

--- k9-dbextract.py
+++ k9-dbextract.py
@@ -7,10 +7,8 @@ conn = sqlite3.connect('preferences_storage')
 cur = conn.cursor()
 
 a = cur.execute("SELECT value FROM preferences_storage WHERE primkey = 'accountUuids'")
-for accID in a.fetchall():
-    print(accID)
-    print("SELECT value FROM preferences_storage WHERE primkey = '" + accID[0] + ".transportURI'")
-    b = cur.execute("SELECT value FROM preferences_storage WHERE primkey = '" + accID[0] + ".transportUri'")
+for accID in a.fetchall()[0][0].split(","):
+    b = cur.execute("SELECT value FROM preferences_storage WHERE primkey = '" + accID + ".transportUri'")
     print(base64.b64decode(b.fetchall()[0][0]))
     
 
